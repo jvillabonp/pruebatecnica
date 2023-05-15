8dfee1fe24d8c45d9dcbc5cb9a95c12b5975f8a1
Jover Villabon <jover.villabon@s4learning.com>
2023-05-14 23:20:08 -0500

    Fix

--- src/sources/js/form.js
+++ src/sources/js/form.js
@@ -2,15 +2,43 @@ const form = document.querySelector('form');
 const result = document.querySelector('#result');
 
 form.addEventListener('submit', function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const fileInput = document.querySelector('#fileInput');
-    const file = fileInput.files[0];
+  const fileInput = document.querySelector('#fileInput');
+  const file = fileInput.files[0];
 
-    Papa.parse(file, {
-        header: true,
-        complete: function(results) {
-            result.innerHTML = JSON.stringify(results.data, null, 8);
+  Papa.parse(file, {
+    header: true,
+    complete: function(results) {
+      const organizations = {};
+
+      results.data.forEach(function(row) {
+        const orgName = row.organizacion;
+        const userName = row.usuario;
+        const role = row.rol;
+
+        if (!organizations[orgName]) {
+          organizations[orgName] = {
+            organization: orgName,
+            users: []
+          };
         }
-    });
+
+        const user = organizations[orgName].users.find(function(u) {
+          return u.username === userName;
+        });
+
+        if (user) {
+          user.roles.push(role);
+        } else {
+          organizations[orgName].users.push({
+            username: userName,
+            roles: [role]
+          });
+        }
+      });
+
+      result.textContent = JSON.stringify(Object.values(organizations), null, 8);
+    }
+  });
 });
